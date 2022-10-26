edd62a703837ab64ff2f0bd072ac74db45609e6a
Darya_Savitskaya <statusqvogerz@gmail.com>
2022-10-26 16:41:30 +0500

    L6T3

---
+++ index.js
@@ -0,0 +1,23 @@
+let q1 = confirm('JavaScript появился в 1995 году?')
+
+if (q1) {
+  alert('Верно! JavaScript появился в 1995 году')
+} else {
+  alert('Неверно! JavaScript появился в 1995 году')
+}
+
+let q2 = confirm('Спецификация JavaScript называется ECMAScript?')
+
+if (q2) {
+  alert('Верно! Спецификация JavaScript называется ECMAScript')
+} else {
+  alert('Неверно! Спецификация JavaScript называется ECMAScript')
+}
+
+let q3 = confirm('JavaScript был создан за 1 месяц?')
+
+if (q3) {
+  alert('Неверно! JavaScript был создан за 10 дней')
+} else {
+  alert('Верно! JavaScript был создан за 10 дней')
+}
